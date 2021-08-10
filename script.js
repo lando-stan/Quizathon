193cc639d56ab3a1cab3217751871ba7f91ecdf6
lando-stan <mohamedguled@yahoo.com>
2021-08-10 15:10:21 -0500

    timer reconfigured

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var timerEl = document.getElementById('clock');
-var mainEl = document.getElementById('');
-var buttonEl = document.getElementById('start-btn')
+var timerEl = document.getElementById("clock");
+var mainEl = document.getElementById("");
+var buttonEl = document.getElementById("start-btn");
 
 //timer variables
 var timeLeft = 120;
@@ -8,30 +8,23 @@ var countDown;
 
 //start quiz fc
 function startQuiz() {
-countDown = setInterval(countdown,1000)
-
-
-
-
+  countDown = setInterval(countdown, 1000);
 }
 
+//SEtting countdown
 function countdown() {
-  
   timeLeft--;
-  timerEl.textContent = timeLeft 
+  timerEl.textContent = timeLeft;
 
   if (timeLeft <= 0) {
     stopQuiz();
-
   }
-
-  
 }
 
+//stopQuiz fc
 function stopQuiz() {
-clearInterval(timeLeft);
+  clearInterval(timeLeft);
+}
 
-
-} 
-
+//Button functionality
 buttonEl.onclick = startQuiz();
